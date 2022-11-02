f60457c765af8f4e81b0f178ec283ba66d81d169
hilmym240297 <hilmym23@gmail.com>
2022-11-02 11:44:53 +0700

    revise myChart function, create js helper for change capability

--- application/modules/rekap/views/cempaka/v_grafik.php
+++ application/modules/rekap/views/cempaka/v_grafik.php
@@ -163,42 +163,4 @@
             }
         }
     }
-
-    function myChart(id, detail, capaian, target) {
-        const ctx = document.getElementById(id);
-
-
-        var dataFirst = {
-            label: detail,
-            data: capaian,
-            lineTension: 0,
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)'
-        };
-
-        var dataSecond = {
-            label: "target ",
-            data: target,
-            lineTension: 0,
-            fill: false,
-            borderColor: 'rgb(255, 51, 51)'
-        };
-
-        var dataCapaianTarget = {
-            labels: ['JAN', 'FEB', 'MAR', 'APR', 'MEI', 'JUN', 'JUL', 'AGT', 'SEP', 'OKT', 'NOV', 'DES'],
-            datasets: [dataFirst, dataSecond]
-        };
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: dataCapaianTarget,
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-
-    }
 </script>
