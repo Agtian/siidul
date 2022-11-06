1171833734d8bdd7d18476a1e18a5301b8d1f3e9
hilmym240297 <hilmym23@gmail.com>
2022-11-06 23:31:08 +0700

    show button tampilkan

--- application/modules/data_imp/views/v_capaian.php
+++ application/modules/data_imp/views/v_capaian.php
@@ -26,7 +26,11 @@
                             </div>
                         </div>
                         <div class="ln_solid"></div>
-
+                        <div class="form-group">
+                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                <button type="submit" class="btn btn-primary">Tampilkan</button>
+                            </div>
+                        </div>
                         <?php echo form_close(); ?>
                     </div>
                 </div>
